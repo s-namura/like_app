57b2fcb6b8c9d72a8c34db10afd579070ecbd977
s-namura <shin-namura@hi.enjoy.ne.jp>
2020-10-18 23:22:52 +0900

    いいね機能実装詳細・一覧ページ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  # post.liked_users で post を「いいね！」しているユーザーの一覧を取得できるようになる
+  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+  # postをuserが「いいね！」している時は「true」，「いいね」していない時は「false」
+  def liked_by?(user)
+    likes.find_by(user_id: user.id).present?
+  end
+
 end
